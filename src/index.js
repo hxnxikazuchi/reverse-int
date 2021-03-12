89f6f137ce697648c63220c68657a491c77b496e
Ruslan Kupzhassarov <kupzhasarov14@gmail.com>
2021-03-12 22:08:38 +0600

    feat: add a code to solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let reversedNum = 0;
+  if(n < 0){
+    n = n *(-1); 
+    }  
+  while(n > 0){     
+    reversedNum = reversedNum * 10 + n % 10;
+    n = Math.floor(n / 10);
+
+  }
+  return reversedNum;
 }
